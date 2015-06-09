352e084e87076b0ecfa4efe796d2712b26646024
Mayank Saxena <mayank.saxena@anktech.co.in>
2015-06-09 14:15:14 +0530

    Project init

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name applicationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the applicationApp
+ */
+angular.module('applicationApp')
+  .controller('ContactController', function ($scope) {
+    $scope.myname = 'mayank.saxena@anktech.co.in';
+  });
