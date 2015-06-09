f78a9b9ff3b93bac09fc0ea5e4989738382921e0
Mayank Saxena <mayank.saxena@anktech.co.in>
2015-06-10 00:46:28 +0530

    Added Loader and Set Min Height of Footer.

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -10,13 +10,21 @@
 angular.module('applicationApp')
   .controller('AboutCtrl', function ($scope, $http) {
     $scope.blogs = [];
+    $scope.isLoading = true;
     $http({ method: 'GET', url: 'http://www.themayanksaxena.com/blogs/list' }).
     success(function (data, status, headers, config) {
         console.log("data", data);
         if(typeof data.blogs !== "undefined")
             $scope.blogs = data.blogs;
+        $scope.isLoading = false;
     }).
     error(function (data, status, headers, config) {
         console.log("data", data);
+        $scope.isLoading = false;
+    });
+    $scope.$on('$viewContentLoaded', function() {
+        //Here your view content is fully loaded !!
+        var minHeight = $(window).height() - $("header").height()-$("footer").height()-20;
+        $(".main").attr("style", "min-height: "+minHeight+"px;");
     });
   });
--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -10,4 +10,11 @@
 angular.module('applicationApp')
   .controller('ContactController', function ($scope) {
     $scope.email = 'mayanksaxena90@gmail.com';
+    $scope.isLoading = true;
+    $scope.$on('$viewContentLoaded', function(){
+    	$scope.isLoading = false;
+        //Here your view content is fully loaded !!
+        var minHeight = $(window).height() - $("header").height()-$("footer").height()-20;
+        $(".main").attr("style", "min-height: "+minHeight+"px;");
+    });
   });
